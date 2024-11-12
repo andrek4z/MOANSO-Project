53bc44e3eebcd3a6aeba4be3cdb51346a97c7ecd
andrek4z <andre.fernandez.98@hotmail.com>
2024-11-12 03:46:27 -0500

    Done

--- Software Minimarket Lily's/Capa_Datos/conexion.cs
+++ Software Minimarket Lily's/Capa_Datos/conexion.cs
@@ -5,39 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
+using System.Configuration;
 
 namespace Capa_Datos
 {
     public class conexion
     {
-            SqlConnection cnx;
-
-        public conexion()
+        public static SqlConnection Conectar()
         {
-            try
-            {
-                cnx = new SqlConnection("Data Source=DESKTOP-5J5OEOD\\SQLEXPRESS;Initial Catalog=MinimarketLogistica_Normalizada;Integrated Security=True");
-                cnx.Open();
-            }
-            catch
-            {
-            }
-        }
-
-        public DataTable ObtenerProductos()
-        {
-            DataTable dt = new DataTable();
-            try
-            {
-                string query = "SELECT * FROM Productos"; 
-                SqlCommand cmd = new SqlCommand(query, cnx);
-                SqlDataAdapter da = new SqlDataAdapter(cmd);
-                da.Fill(dt);
-            }
-            catch
-            {
-            }
-            return dt;
+            SqlConnection conex = new SqlConnection();
+            conex.ConnectionString = "Data Source = .; Initial Catalog = MinimarketLogistica_Normalizada;" + "Integrated Security = true";
+            return conex;
         }
     }
 }
